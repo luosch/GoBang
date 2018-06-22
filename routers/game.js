55f52b807dc3ebdca8f40408451161f7da6cc318
luosch <i@lsich.com>
2018-06-22 18:00:09 +0800

    [Add] 对战接口

--- routers/game.js
+++ routers/game.js
@@ -4,11 +4,43 @@ let router = Router({
   'prefix': '/game' 
 });
 
-
 router.get('/', async (ctx, next) => {
   await ctx.render('game', {
     'walletAddress': ctx.session.walletAddress
   });
+});
+
+router.get('/match/:id', async (ctx, next) => {
+  // let userId = ctx.request.body['userId'] || ''
+  // let password = ctx.request.body['password'] || ''
+
+  console.log("ctx.request.body", ctx.request.body);
+  ctx.body = {
+    'status': 'fail',
+    'message': '操作失败'
+  }
+});
+
+router.post('/add', async (ctx, next) => {
+  // let userId = ctx.request.body['userId'] || ''
+  // let password = ctx.request.body['password'] || ''
+
+  console.log("ctx.request.body", ctx.request.body);
+  ctx.body = {
+    'status': 'fail',
+    'message': '操作失败'
+  }
+});
+
+router.post('/add', async (ctx, next) => {
+  // let userId = ctx.request.body['userId'] || ''
+  // let password = ctx.request.body['password'] || ''
+
+  console.log("ctx.request.body", ctx.request.body);
+  ctx.body = {
+    'status': 'fail',
+    'message': '操作失败'
+  }
 });
 
 module.exports = router;
